7ef9bd2830e8da4c5084b18eaa6fa476c67f0fd5
khush <khush@ubuntu.myguest.virtualbox.org>
2023-03-15 18:20:06 +0530

    Linux Training Day 3

---
+++ linux_training/day_3/temp2/file1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int main(){
+	int a = 30,b = 6;
+	printf("\nAddition of a and b is %d\n",add(a,b));
+	printf("\nSubtraction of a and b is %d\n",sub(a,b));
+	return 1;
+}
